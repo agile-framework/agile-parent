f9b053b937a01b903415e4c132106986e88b7ea4
mydeathtrial <tongmeng19900905>
2018-06-21 17:57:05 +0800

    spring cloud2 + Agile boot

---
+++ agile-eureka-server/src/main/java/com/agileframework/agileeurekaserver/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.agileframework.agileeurekaserver;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+/**
+ * Created by 佟盟 on 2018/6/21
+ */
+@EnableWebSecurity
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+}
